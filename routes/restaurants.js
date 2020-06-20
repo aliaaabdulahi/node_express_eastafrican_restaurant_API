bec67f63bf269afd0fcc1ea4b6b18294570620eb
Alia Ali <39501230+aliaaabdulahi@users.noreply.github.com>
2020-06-20 18:18:38 -0400

    Update restaurants.js

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const { getRestaurants, getRestaurant, createRestaurant, updateRestaurant, deleteRestaurant } = require('../controllers/restaurants');
 
 const router = express.Router();
+
 //class used to create modular, mountable route handlers. complete middleware and routing system
 
 router.route('/').get(getRestaurants).post(createRestaurant);
